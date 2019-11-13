6be415e396f013da566ce0eb6ca0e3e50af64ff1
parisChatz <parischatz94@gmail.com>
2019-11-13 16:04:42 +0000

    update

--- Task_1A.py
+++ Task_1A.py
@@ -37,17 +37,19 @@ class network():
         
         # self.CPTs["order"] = ["D", "T"]
         # self.CPTs["parents"] = {"D": None, "T": "D"}
-        self.CPTs["C"]={"+c":0.50, "-c":0.50}
-        self.CPTs["S"]={"+s|+c":0.10, "-s|+c":0.90, 
-                "+s|-c":0.50, "-s|-c":0.50}
-        self.CPTs["R"]={"+r|+c":0.80, "-r|+c":0.20, 
-                "+r|-c":0.20, "-r|-c":0.80}
-        self.CPTs["W"]={"+w|+s+r":0.99, "-w|+s+r":0.01, 
-                "+w|+s-r":0.90, "-w|+s-r":0.10,
-                "+w|-s+r":0.90, "-w|-s+r":0.10,
-                "+w|-s-r":0.00, "-w|-s-r":1.00}
-        self.CPTs["order"]=["C", "S", "R", "W"]
-        self.CPTs["parents"]={"C":None, "S":"C", "R":"C", "W":"S,R"}
+        self.CPTs["B"]={"+b":0.001, "-b":0.999}
+        self.CPTs["E"]={"+e":0.002, "-e":0.998}
+        self.CPTs["A"]={"+a|+b+e":0.95, "-a|+b+e":0.05, 
+                "+a|+b-e":0.94, "-a|+b-e":0.06,
+                "+a|-b+e":0.29, "-a|-b+e":0.71,
+                "+a|-b-e":0.001, "-a|-b-e":0.999}
+        self.CPTs["J"]={"+j|+a":0.90, "-j|+a":0.10, 
+                "+j|-a":0.05, "-j|-a":0.95}
+        self.CPTs["M"]={"+m|+a":0.70, "-m|+a":0.30, 
+                "+m|-a":0.01, "-m|-a":0.99}
+        self.CPTs["order"]=["B", "E", "A", "J", "M"]
+        self.CPTs["parents"]={"B":None, "E":None, "A":"B,E", "J":"A", "M":"A"}
+
 
     def sampleVariable(self, CPT, conditional):
         sampledValue=None
@@ -89,8 +91,11 @@ class network():
         normalized = {}
         prob_sum = float(sum(unormalized_probs.values()))
         for sign, value in unormalized_probs.items():
-            normalized[sign] = value / prob_sum
-
+            if prob_sum != 0:
+                normalized[sign] = value / prob_sum
+            else:
+                normalized[sign] = 0
+        print(unormalized_probs)
         return normalized
 
     def is_consistent(self, evidence, random_query):
@@ -105,19 +110,18 @@ class network():
 
     def rej_sampling(self,reps = 100000):  
         N = {"positive": 0, "negative": 0}
-        X, e = parsing().parse_data("P(+w|+s,-r)")
-        random_query = self.sampleVariables()
-              
+        X, e = parsing().parse_data("P(a|-b,-e)")
         for j in range(1, reps):
             random_query = self.sampleVariables()
+            print(random_query,X,e,self.is_consistent(e,random_query))
             if self.is_consistent(e,random_query):
-                if("+" in random_query[0]):
+                if('+'+X[0] in random_query):
                     N["positive"] += 1
-                if("-" in random_query[0]):
+                else:
                     N["negative"] += 1
 
         k = self.normalize(N)
-        print("rej_sampling P(d|t): ",k)
+        print("rej_sampling P(-a|-b-e): ",k)
         
 
     def bayes_eq(self):
