098724305bb0e421e509d95d12117672410ddee0
parisChatz <parischatz94@gmail.com>
2019-12-04 10:30:54 +0000

    readcpts update

--- read_cpt_from_csv.py
+++ read_cpt_from_csv.py
@@ -5,6 +5,35 @@ from parsing import parsing
 class read_cpts_from_csv():
     def __init__(self,net):
         self.net = net
+        self.df = pd.read_csv('lucas0_train.csv')
+        self.row, self.col = self.df.shape
+        self.df.replace(0,"-",inplace=True)
+        self.df.replace(1,"+",inplace=True)
+        self.df.columns = ['S','YF','AN','PP','G','AD','BED','CO','F','AL','CA','LC']
+
+    def find_prob0(self,var_nosign,variables,conditions=None,new_cond=None):
+        sign = variables[0]
+        count = self.df[var_nosign].value_counts()
+        count = count[sign]
+        return (count+1)/(self.row+2)
+
+    def find_prob1(self,var_nosign,variables,conditions=None,new_cond=None):
+        sign = variables[0]
+        cond_sign = conditions[0]
+        kounter = 0
+        kounter2 = 0
+        for num, item in enumerate(self.df[variables[1:].upper()]):
+            if item == sign and self.df[conditions[1:].upper()][num]==cond_sign:
+                kounter+=1
+            if self.df[conditions[1:].upper()][num]==cond_sign:
+                kounter2+=1
+        return(kounter+1)/(kounter2+2)
+
+    def find_prob2(self,var_nosign,variables,conditions=None,new_cond=None):
+
+
+
+
 
     def read_cpts(self):
         variables = []
@@ -12,16 +41,19 @@ class read_cpts_from_csv():
         var_nosign = []
         cond_no_sign = []
         new_cond = []
-        df = pd.read_csv('lucas0_train.csv')
+        con = []
         for node in self.net["order"]:
             keylist = list(self.net[node].keys())
             for cond_prob in range(len(keylist)):
                 variable, condition = parsing().parse_querry(keylist[cond_prob])
                 variables.append(variable)
-                conditions.append(condition)
+                if condition is None:
+                    conditions.append(None)
+                else:
+                    conditions.append(condition)
                 var_nosign.append(variable[1:].upper())
-                if condition==None:
-                    cond_no_sign.append(None)
+                if condition is None:
+                    cond_no_sign.append([None])
                 else:
                     cond_no_sign.append(parsing().parse_evidence(condition))
         new_cond = cond_no_sign
@@ -29,17 +61,29 @@ class read_cpts_from_csv():
         for i in range(len(cond_no_sign)):
             if cond_no_sign[i] is not None:
                 for k in range(len(cond_no_sign[i])):
-                    result = conditions[i].find(cond_no_sign[i][k])
-                    new_cond[i][k]=conditions[i][result-1]+cond_no_sign[i][k]
+                    if conditions[i] != cond_no_sign[i][k]:
+                        result = conditions[i].find(cond_no_sign[i][k])
+                        new_cond[i][k]=conditions[i][result-1]+cond_no_sign[i][k]
                     # print(result,conditions[i],cond_no_sign[i][k])
-            print(var_nosign[i],variables[i],conditions[i])
 
-            # if  cond_no_sign[i] == variables[i]:
-            #     cond_no_sign[i] = None
-            # print(var_nosign[i], variables[i],cond_no_sign[i])
-            # print(cond_no_sign[i],cond_no_sign[i])
-        
-        # for i in range(len(variables)):
+        for i in range(len(variables)):
+            if conditions[i] is  None:
+                con.append(variables[i])
+            elif conditions[i] is not None:
+                con.append(variables[i]+"|"+conditions[i])
+
+            print(var_nosign[i],variables[i],new_cond[i],conditions[i],con[i])
+
+
+        size = len(var_nosign)
+        for i in range(size):
+            if conditions[i] is  None:
+                self.net[var_nosign[i]][con[i]] = self.find_prob0(var_nosign[i],variables[i])
+                print(var_nosign[i],con[i],self.net[var_nosign[i]][variables[i]])
+            else:
+                self.net[var_nosign[i]][con[i]] = self.find_prob1(var_nosign[i],variables[i],conditions[i], new_cond[i])
+                print(var_nosign[i],con[i],self.net[var_nosign[i]][con[i]])
+            
 
 
 
